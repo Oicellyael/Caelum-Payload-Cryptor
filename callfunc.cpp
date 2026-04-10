8d14c658c7d69c3489528df2c8d3e9a064872d79
Oicellyael <149580998+Oicellyael@users.noreply.github.com>
2026-04-10 10:19:48 +0200

    C++ source for the shellcode

---
+++ callfunc.cpp
@@ -0,0 +1,13 @@
+﻿#include <windows.h>
+
+
+int main() {
+    int hp = 100;
+    while (hp > 0) {
+        hp -= 10;
+        Sleep(100); 
+    }
+
+    MessageBoxA(NULL, "Payload Executed! HP is 0.", "C++ Agent", MB_OK | MB_ICONEXCLAMATION);
+    return 0;
+}
